42d8799fa87a05eaa727066339fbb8baaf0ad16c
Alejandro-Juarez <fj21003@ues.edu.sv>
2025-10-22 18:38:26 -0600

    Guardar cambios

---
+++ Sistema de gestion de atencion al cliente web/src/java/base_datos/ConexionBD.java
@@ -0,0 +1,36 @@
+package base_datos;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author Adam
+ */
+public class ConexionBD {
+    private static final String URL ="jdbc:postgresql://localhost:5432/proyecto_prnIII";
+    private static final String USER="postgres";
+    private static final String PASSWORD="Admin";
+    //
+    public static Connection conectar(){
+        Connection conn=null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            //establecer conexion
+            conn=DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexion exitosa a PostgreSQL");
+        } catch (ClassNotFoundException e) {
+            System.out.println("No se encontró el driver JDBC de PostgreSQL");
+            e.printStackTrace();
+        }catch(SQLException e){
+            System.out.println("Error al conectar a la BD");
+            e.printStackTrace();
+        }
+        return conn;
+    }
+    
+}
